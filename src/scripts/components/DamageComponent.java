dfee8df6d8097f04b5c8bb2635153e0121c4673f
fireberi <99780092+fireberi@users.noreply.github.com>
2024-07-07 20:49:53 +1000

    clean up syntax

--- src/scripts/components/DamageComponent.java
+++ src/scripts/components/DamageComponent.java
@@ -1,13 +1,13 @@
 package scripts.components;
 
-import scripts.util.DamageTypes;
+import scripts.util.DamageTypes.Damage;
 
 public class DamageComponent {
 
-    public DamageTypes.Damage[] effects;
+    public Damage[] effects;
     public float[] values;
 
-    public DamageComponent(DamageTypes.Damage[] effects, float[] values) {
+    public DamageComponent(Damage[] effects, float[] values) {
         this.effects = effects;
         this.values = values;
     }
