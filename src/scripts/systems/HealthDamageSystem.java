25f819ff8694afc2306002b093e57fc87c67e037
fireberi <99780092+fireberi@users.noreply.github.com>
2024-07-09 20:00:34 +1000

    fix entities being instant killed when applying Damage.INSTANT effect

--- src/scripts/systems/HealthDamageSystem.java
+++ src/scripts/systems/HealthDamageSystem.java
@@ -1,6 +1,7 @@
 package scripts.systems;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 import dev.dominion.ecs.api.Dominion;
 
@@ -29,9 +30,12 @@ public class HealthDamageSystem implements Runnable {
             ArrayList<Float> values = hth.values;
 
             int i = 0;
+            boolean[] markDeletes = new boolean[effects.size()];
+
             for (Damage d : effects) {
                 if (d == Damage.INSTANT) {
                     hth.health = Math.max(0f, hth.health - values.get(i));
+                    markDeletes[i] = true;
                 }
 
                 if (hth.health <= 0) {
@@ -45,6 +49,24 @@ public class HealthDamageSystem implements Runnable {
                     }
                 }
 
+                i++;
+            }
+
+            // reset effects and values
+            ListIterator<Damage> eIter = effects.listIterator();
+            i = 0;
+            while (eIter.hasNext()) {
+                if (markDeletes[i]) {
+                    effects.remove(i);
+                }
+                i++;
+            }
+            ListIterator<Float> vIter = values.listIterator();
+            i = 0;
+            while (vIter.hasNext()) {
+                if (markDeletes[i]) {
+                    values.remove(i);
+                }
                 i++;
             }
         });
